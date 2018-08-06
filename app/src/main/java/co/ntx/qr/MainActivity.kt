76e88111515631f071cf88892639a524f4fdd82d
Xtn <xtnaj@qq.com>
2018-08-06 12:18:01 +0800

    跳转扫描界面

--- app/src/main/java/co/ntx/qr/MainActivity.kt
+++ app/src/main/java/co/ntx/qr/MainActivity.kt
@@ -4,42 +4,34 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import com.google.zxing.integration.android.IntentIntegrator
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.toast
 
 /**
  * xtn
  */
 class MainActivity : AppCompatActivity() {
 
-//    private var tv : TextView? = null
-//    private var camera : IntentIntegrator? = null
-//    private var scanButton : Button? = null
-    private val tv = findViewById<TextView>(R.id.text1)
     private val camera = IntentIntegrator(this)
-    private val scanButton = findViewById<Button>(R.id.scan)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        init()
-        tv.text = "????"
+        text1.text = "????"
         startQr()
     }
 
 
     private fun startQr(){
         Permission().getPermission(this)
-        scanButton.setOnClickListener{
+        scan.setOnClickListener{
             camera.initiateScan()
+            toast("点击")
         }
-//        camera.initiateScan()
 
     }
 
 
-//    private fun init(){
-//        tv = findViewById(R.id.text1)
-//        scanButton = findViewById(R.id.scan)
-//        camera = IntentIntegrator(this)
-//    }
 }
